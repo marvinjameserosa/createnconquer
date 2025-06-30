e4b1e14662ed61920760a837080f1127ce7d367f
Marvin James A. Erosa <104268552+marvinjameserosa@users.noreply.github.com>
2025-06-30 16:20:34 +0800

    fix: add coma in eslint file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,7 @@ const eslintConfig = [
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-implicit-any-catch': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
-      '@typescript-eslint/no-unsafe-argument': 'off'
+      '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       'react/no-unescaped-entities': 'off'
     }
